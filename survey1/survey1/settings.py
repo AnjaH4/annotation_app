29d507a9d154ae36a2e54817443c9e1b023d01da
AnjaH4 <ah40785@student.uni-lj.si>
2025-06-16 21:27:04 +0200

    settings for production, requirements.txt

--- survey1/survey1/settings.py
+++ survey1/survey1/settings.py
@@ -15,6 +15,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,17 +25,19 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sol4ua!3yh@-^=f+ksx4682$d8%)(nroby)bce(x7ll*4+w1!o'
+#SECRET_KEY = 'sol4ua!3yh@-^=f+ksx4682$d8%)(nroby)bce(x7ll*4+w1!o'
+SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 # DEBUG = False
 
 
 ALLOWED_HOSTS = [
-    '127.0.0.1',
+    '193.2.72.133',
+    'deepfake.fri.uni-lj.si'
 ]
 
 
@@ -86,8 +89,12 @@ WSGI_APPLICATION = 'survey1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': '',
     }
 }
 
