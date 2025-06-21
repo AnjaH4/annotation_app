28549f849453b5464c3316ec327a1fd31930a670
Debian <debian@deepfake-labeler.fri.uni-lj.si>
2025-06-21 07:30:38 +0000

    working slovenian version

--- survey1/surveyapp/urls.py
+++ survey1/surveyapp/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
-from surveyapp.views import introPage, familiarizationPage, helpPage, exampleTask, \
-     mainQuPage, survey_complete
+from . import views  # Using a relative import is best practice here
 
 urlpatterns = [
-    path('intro', introPage, name='intro'),
-    path('exampleTask', exampleTask, name='exampleTask'),
-    path('introHelp', helpPage, name='introHelp'),
-    path('introFamiliarization', familiarizationPage, name='introFamiliarization'),
-    path('main', mainQuPage, name='main1'),
-    path('submit_answer', mainQuPage, name='submit_answer'),
-    path('survey_complete', survey_complete, name='survey_complete')
+    path('intro/', views.introPage, name='intro'),
+    path('exampleTask/', views.exampleTask, name='exampleTask'),
+    path('introHelp/', views.helpPage, name='introHelp'),
+    path('introFamiliarization/', views.familiarizationPage, name='introFamiliarization'),
+    path('main/', views.mainQuPage, name='main1'),
+    path('submit_answer/', views.mainQuPage, name='submit_answer'),
+    path('survey_complete/', views.survey_complete, name='survey_complete'),
 ]
 
 handler500 = 'surveyapp.views.handler500'
